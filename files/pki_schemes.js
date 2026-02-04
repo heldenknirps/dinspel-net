cac603680cee81046b44ae78f054244dbf7902b0
heldenknirps <heldenknirps@gmail.com>
2026-02-04 14:01:49 +0100

    Add files via upload

---
+++ files/pki_schemes.js
@@ -0,0 +1,11 @@
+const rawPKIschemes = `
+Scheme,sk,pk,ct,nist-sec-level,kem, link
+KYBER,1632,800,768,1,1,""
+HQC,7317,7245,14485,5,1,""
+ECDHE,,,,classic,0,""
+FFDHE,,,,classic,0,""
+RSA,,,,classic,0,""
+RSA-OAEP,,,,classic,0,""
+FrodoKEM,,,,classic,0,""
+X-Wing,,,,classic,0,""
+`;
